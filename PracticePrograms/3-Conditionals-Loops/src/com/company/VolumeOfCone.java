7be38c1727a71e656afa57d3440568f26affdaeb
Aman <amanvermaspace@gmail.com>
2022-02-14 11:02:07 +0530

    Solve another program

--- PracticePrograms/3-Conditionals-Loops/src/com/company/VolumeOfCone.java
+++ PracticePrograms/3-Conditionals-Loops/src/com/company/VolumeOfCone.java
@@ -1,7 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
 public class VolumeOfCone {
+
+    public static final DecimalFormat df = new DecimalFormat("0.00");
     public static void main(String[] args){
 
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter height of cone: ");
+        float height = in.nextFloat();
+        System.out.print("Enter radius of circle: ");
+        float radius = in.nextFloat();
+
+        double volume = Math.PI * Math.pow(radius, 2) * (height/3);
+        System.out.println(df.format(volume) + " sq. units");
     }
 }
