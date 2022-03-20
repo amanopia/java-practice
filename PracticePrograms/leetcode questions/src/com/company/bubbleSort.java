8b584bcb7c2a76dabb75d5563740669c05c37b18
Aman <amanvermaspace@gmail.com>
2022-03-20 23:58:29 +0530

    bubble sort improved

--- PracticePrograms/leetcode questions/src/com/company/bubbleSort.java
+++ PracticePrograms/leetcode questions/src/com/company/bubbleSort.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class bubbleSort {
     public static void main(String[] args){
 
-        int[] arr = {-1,7,-32,89};
+        int[] arr = {1,2,3,5,4};
+        boolean swapped;
 //        System.out.print(Arrays.toString(bubble(arr)));
         for(int i = 0; i < arr.length; i++){
+            swapped = false;
             // for each step, max item will come at the last respective index
             for(int j = 1; j<arr.length-i; j++){
                 if(arr[j] < arr[j-1]){
@@ -15,24 +17,35 @@ public class bubbleSort {
                     arr[j-1] = arr[j];
                     arr[j] = temp;
                     System.out.println(Arrays.toString(arr));
+                    swapped = true;
                 }
 
             }
+            if(!swapped){
+                break;
+            }
             System.out.println("----------");
         }
+        System.out.print("final sorted array: ");
+        System.out.println(Arrays.toString(arr));
     }
     static int[] bubble(int[] arr){
 
         int temp = 0;
-
-        for(int i = 0; i < arr.length - 1; i++){
-            for(int j = 1; j < arr.length; j++){
+        boolean swapped;
+        for(int i = 0; i < arr.length; i++){
+            swapped = false;
+            for(int j = 1; j < arr.length-i; j++){
                 if(arr[j]<arr[j-1]){
                     temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
 
+            }
+            if(!swapped){
+                break;
             }
         }
         return arr;
