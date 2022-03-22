d63b2e2164c974ea430498c0a641ae1d31b5d35a
Aman <amanvermaspace@gmail.com>
2022-03-23 00:01:06 +0530

    add a swapping function

--- PracticePrograms/leetcode questions/src/com/company/SelectionSorting.java
+++ PracticePrograms/leetcode questions/src/com/company/SelectionSorting.java
@@ -5,19 +5,23 @@ import java.util.Arrays;
 public class SelectionSorting {
     public static void main(String[] args){
 
-        int[] arr = {4,5,3,1,2 };
+        int[] arr = {-3,-32,-10,45,10,11};
 
         for(int index = 0; index < arr.length; index++ ){
             int last = arr.length - index - 1;
 
             int largestIndex = maximum(arr, last);
 
-            int temp;
-            temp = arr[largestIndex];
-            arr[largestIndex] = arr[last];
-            arr[last] = temp;
+            swap(arr, last, largestIndex);
         }
         System.out.print(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int last, int largestIndex){
+        int temp;
+        temp = arr[largestIndex];
+        arr[largestIndex] = arr[last];
+        arr[last] = temp;
+
     }
     static int maximum(int[] arr, int last){
         int start = 0;
