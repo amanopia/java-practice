4c2ac9c09f1a2b0201f931d4f9a9e742cf813790
Aman <amanvermaspace@gmail.com>
2022-04-02 20:37:11 +0530

    correct first and last occurrence code

--- PracticePrograms/leetcode questions/src/com/company/FirstAndLastOccurrence.java
+++ PracticePrograms/leetcode questions/src/com/company/FirstAndLastOccurrence.java
@@ -3,33 +3,41 @@ package com.company;
 import java.util.Arrays;
 public class FirstAndLastOccurrence {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //                   0 1 2 3 4 5 6 7 8  9 10 11
-        int[] arr = {1,3,3,5,6,7,8,8,9,10,11,11};
-        int target = 3;
+        int[] arr = {1, 3, 3, 5, 6, 7, 8, 8, 9, 10, 11, 11};
+        int target = 8;
 
-        System.out.print(Arrays.toString(firstAndLastOccurrence(arr, target)));
+        int firstOccurrence = firstAndLastOccurrence(arr, target, true);
+        int lastOccurrence = firstAndLastOccurrence(arr, target, false);
+
+        System.out.print(firstOccurrence);
+        System.out.print(lastOccurrence);
     }
-    static int[] firstAndLastOccurrence(int[] arr, int target){
+
+    // firstLast(arr, target, true) // for first occurrence
+    static int firstAndLastOccurrence(int[] arr, int target, boolean firstOccurrence) {
 
         int start = 0;
         int end = arr.length - 1;
-        int empStart = 0;
+        int ans = -1;
 //        int empEnd = 0;
 
-        while(start <= end){
-            int mid = start + (end - start)/2;
-            if(target > arr[mid]){
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
                 start = mid + 1;
-            }else if(target < arr[mid]){
+            } else if (target < arr[mid]) {
                 end = mid - 1;
             } else {
-                if(arr[mid] == arr[mid+1]){
-                    return new int[] {mid-1, mid};
+                ans = mid;
+                if (firstOccurrence == true) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
                 }
             }
         }
-
-        return new int[] {-1, -1};
+        return ans;
     }
 }
