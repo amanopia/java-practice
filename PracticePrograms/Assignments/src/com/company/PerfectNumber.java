9ada02ad1038f7544bafc6d83abba41c5e379641
Aman <amanvermaspace@gmail.com>
2022-05-12 11:49:01 +0530

    new programs

--- PracticePrograms/Assignments/src/com/company/PerfectNumber.java
+++ PracticePrograms/Assignments/src/com/company/PerfectNumber.java
@@ -1,4 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class PerfectNumber {
+    public static void main(String[] args){
+
+        Scanner  in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = in.nextInt();
+
+        int factSum = 0;
+        for(int index = 1; index<num; index++){
+            if(num%index == 0){
+                factSum = factSum + index;
+            }
+        }
+        System.out.println(factSum);
+        if(factSum == num){
+            System.out.print("Perfect Number");
+        } else {
+            System.out.print("Not a perfect number");
+        }
+    }
 }
