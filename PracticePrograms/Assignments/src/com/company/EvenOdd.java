164848cf15e4e20222ed6bd7a38100da82f57bdd
Aman <amanvermaspace@gmail.com>
2022-05-13 16:12:37 +0530

    even odd program done

--- PracticePrograms/Assignments/src/com/company/EvenOdd.java
+++ PracticePrograms/Assignments/src/com/company/EvenOdd.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 public class EvenOdd {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int a = in.nextInt();
+        System.out.println(EvenOd(a));
+    }
+    public static boolean EvenOd(int a){
+        boolean even;
+
+        if(a%2 == 0){
+            even = true;
+        } else {
+            even = false;
+        }
+        return even;
     }
 }
