c0a43edd067e9f9ff81fd4e4b6d6aa6c4e18771e
Aman <amanvermaspace@gmail.com>
2022-06-03 12:54:47 +0530

    Find minimum

--- PracticePrograms/Assignments/src/LinearSearch/FindMinimumNumber.java
+++ PracticePrograms/Assignments/src/LinearSearch/FindMinimumNumber.java
@@ -1,5 +1,21 @@
 package LinearSearch;
 
 public class FindMinimumNumber {
-    
+    public static void main(String[] args){
+        int[] arr = {1,2,3,-43,4,5,6,7,8};
+        System.out.println(min(arr));
+    }
+    static int min(int[] arr){
+        // base case
+        if(arr.length == 0){
+            return -1;
+        }
+        int minimum = arr[0];
+        for(int i:arr){
+            if(i < arr[0]){
+                minimum = i;
+            }
+        }
+        return minimum;
+    }
 }
